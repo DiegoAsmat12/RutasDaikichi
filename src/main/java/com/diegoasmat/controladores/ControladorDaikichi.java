52e645c77c6c60e1520f0c3e06b9426ba4e3fd25
DiegoAsmat12 <diegoas1205@gmail.com>
2022-02-18 16:58:35 -0500

    Variables de ruta añadidas

--- src/main/java/com/diegoasmat/controladores/ControladorDaikichi.java
+++ src/main/java/com/diegoasmat/controladores/ControladorDaikichi.java
@@ -1,5 +1,6 @@
 package com.diegoasmat.controladores;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,18 @@ public class ControladorDaikichi {
 	@RequestMapping(value="/tomorrow",method=RequestMethod.GET)
 	public String tomorrow() {
 		return "¡Mañana, una oportunidad aparecerá, así que debes estar listo para recibir nuevas ideas!";
+	}
+	
+	@RequestMapping(value="/travel/{ciudad}", method = RequestMethod.GET)
+	public String viaje(@PathVariable("ciudad")String ciudad) {
+		return "¡Felicitaciones! ¡Pronto viajaras a "+ciudad+"!";
+	}
+	
+	@RequestMapping(value="/lotto/{numero}", method = RequestMethod.GET)
+	public String lotto(@PathVariable("numero")int numero) {
+		if(numero%2==0) {
+			return "Harás un viaje en un futuro cercano, pero ten cuidado con las ofertas tentadoras.";
+		}
+		return "Has disfrutado de los frutos de tu trabajo, pero ahora es un buen momento para pasar tiempo con familiares y amigos.";
 	}
 }
